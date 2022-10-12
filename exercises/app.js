f25db118fd1a1a920fb84a2e600dc228b42a1dc6
Lucy_glb <shuiqiong.lu@colorado.edu>
2022-10-12 16:27:16 -0600

    message

---
+++ exercises/app.js
@@ -0,0 +1,12 @@
+var num=90;
+if (num<=60){
+    console.log("Failure");
+}else if(num<=70){
+    console.log("good");
+}else if(num<=80){
+    console.log("better");
+}else if(num<=100){
+    console.log("awesome");
+}else{
+    console.log("unreasonable number");
+}
